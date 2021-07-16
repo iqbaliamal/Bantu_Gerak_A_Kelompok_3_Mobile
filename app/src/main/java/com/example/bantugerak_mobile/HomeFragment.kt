1cf3c91fc300127b503ea042b8ae9df096ab64f3
iqbaliamal <iqbaliamal@gmail.com>
2021-07-17 01:15:57 +0700

    header, slider

--- app/src/main/java/com/example/bantugerak_mobile/HomeFragment.kt
+++ app/src/main/java/com/example/bantugerak_mobile/HomeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,14 +31,27 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val imageSlider = view.findViewById<ImageSlider>(R.id.image_slider)
+        val imageList = ArrayList<SlideModel>()
+
+        imageList.add(SlideModel("https://cdn1-production-images-kly.akamaized.net/Lp4ZMNdzQ89iphOeDwpz1A0UV5g=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2703835/original/060532300_1547463460-macam-macam_bencana_alam.jpg"))
+        imageList.add(SlideModel("https://www.its.ac.id/news/wp-content/uploads/sites/2/2018/12/letusan-gunung-krakatau_20170923_174854.jpg"))
+        imageList.add(SlideModel("https://apahabar.com/wp-content/uploads/2020/06/1e57a22c-c7a3-43fb-b7c4-ad3e5bdd11f8.jpg"))
+        imageList.add(SlideModel("https://awsimages.detik.net.id/visual/2020/01/02/05686423-6018-4448-b20e-9cc415885723.png?w=650"))
+
+        imageSlider.setImageList(imageList,ScaleTypes.FIT)
+        return view
+//        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -56,4 +73,5 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
 }
--- app/src/main/java/com/example/bantugerak_mobile/MainActivity.kt
+++ app/src/main/java/com/example/bantugerak_mobile/MainActivity.kt
@@ -3,21 +3,32 @@ package com.example.bantugerak_mobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 import androidx.collection.arraySetOf as arraySetOf1
 
 class MainActivity : AppCompatActivity() {
+//    lateinit var myDb: MyDatabase
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
 
-        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
+
+//        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.fragment)
@@ -26,5 +37,17 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottomNavigationView.setupWithNavController(navController)
+
+//        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+//        val imageList = ArrayList<SlideModel>()
+//
+//        imageList.add(SlideModel("https://cdn1-production-images-kly.akamaized.net/Lp4ZMNdzQ89iphOeDwpz1A0UV5g=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2703835/original/060532300_1547463460-macam-macam_bencana_alam.jpg"))
+//        imageList.add(SlideModel("https://www.its.ac.id/news/wp-content/uploads/sites/2/2018/12/letusan-gunung-krakatau_20170923_174854.jpg"))
+//        imageList.add(SlideModel("https://apahabar.com/wp-content/uploads/2020/06/1e57a22c-c7a3-43fb-b7c4-ad3e5bdd11f8.jpg"))
+//        imageList.add(SlideModel("https://awsimages.detik.net.id/visual/2020/01/02/05686423-6018-4448-b20e-9cc415885723.png?w=650"))
+//
+//        imageSlider.setImageList(imageList,ScaleTypes.FIT)
     }
+
+
 }
